cabf74fb821d856b85bd4a5dda1fead6a9d40ba9
Andre Stern <andrestern88@users.noreply.github.com>
2025-07-05 11:57:12 +0100

    initial commit

---
+++ src/main/java/com/example/barclays/BarclaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.barclays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarclaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarclaysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barclays/BarclaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barclays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarclaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
